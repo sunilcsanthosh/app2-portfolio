f12cf2acf51dd9790ba05c60e24fdb20f151cc0f
sunilcsanthosh <sunilcsanthosh@gmail.com>
2023-05-12 16:27:02 +0400

    code updated

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(layout= "wide")
+
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Sunil Santhosh")
+    content =""" 
+    Hi, This is me Sunil Santhosh blablabla
+    """
+    st.info(content )
