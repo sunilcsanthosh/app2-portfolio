54a19d1fa518a90ed9e36d13fdb994e8870eb673
sunilcsanthosh <sunilcsanthosh@gmail.com>
2023-05-14 15:32:00 +0400

    update

--- pages/Contact_me.py
+++ pages/Contact_me.py
@@ -5,9 +5,17 @@ st.header("Contact Me")
 
 with st.form(key="email_form"):
     user_email = st.text_input("Your email address")
-    message = st.text_input("Your Message")
-    message = message + "\n" + user_email
+    raw_message = st.text_input("Your Message")
+    #message = message + "\n" + user_email
+    message = f"""
+    Subject: New email from {user_email}
+    
+    From: {user_email}
+    {raw_message}
+    """
+
     button = st.form_submit_button("Submitt")
 
     if button:
         send_email(message)
+        st.info("Your email was sent sucessfully")
